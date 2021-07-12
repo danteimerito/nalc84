92f4476d0887505e8ea38bea73d33cb4db8a6b84
danteimerito <danteimerito@gmail.com>
2021-07-11 23:34:34 -0400

    modified retirement page

--- page-retirement.php
+++ page-retirement.php
@@ -1,14 +1,6 @@
 
 <?php get_header(); ?>
 
-<div class="row  color-reverse-darker"> 
-    <div class="container page-header-spacer"> 
-        <h3> <?php the_title(); ?> </h3>
-    </div>
-</div>
-
-
-
 
 
 <div class="container nav-spacer">
@@ -31,7 +23,9 @@
 
       <div class="row publications">
         <h2>Retirement Publications</h2>
+
         <ul>
+
           <li>
             <div class="pub-image">
               <a href="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/RetirementGuide-FERS.pdf" target="blank">
@@ -43,42 +37,42 @@
               Questions & Answers on FERS Federal Employees Retirement System
             </div>
           </li>
+
           <li>
             <div class="pub-image">
-              <a href={pdf2} target="blank">
-                <Image fluid={data.pubPreparingForRetirement.childImageSharp.fluid} />
+              <a href="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/Preparing-for-Retirement.pdf" target="blank">
+                <img src="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/Preparing-for-Retirement.pdf" />
               </a>
             </div>
 
-            <a href={pdf2} target="blank" class="waves-effect waves-light btn-small pub-button">View</a>
-            <a href={pdf2} class="waves-effect waves-light btn-small pub-button" download>Download</a>
+            <a href="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/Preparing-for-Retirement.pdf" target="blank" class="waves-effect waves-light btn-small pub-button">View</a>
 
             <div class="pub-description">
               Preparing For Retirement; Checklist for letter carriers during their final year at work</div>
 
           </li>
+
+
           <li>
             <div class="pub-image">
-              <a href={pdf3} target="blank">
-                <Image fluid={data.pubRetiredDies.childImageSharp.fluid} />
+              <a href="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/when-a-retired-letter-carrier-dies.pdf" target="blank">
+                <img src="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/when-a-retired-letter-carrier-dies.png"/>
               </a>
             </div>
-            <a href={pdf3} target="blank" class="waves-effect waves-light btn-small pub-button">View</a>
-            <a href={pdf3} class="waves-effect waves-light btn-small pub-button" download>Download</a>
+            <a href="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/when-a-retired-letter-carrier-dies.pdf" target="blank" class="waves-effect waves-light btn-small pub-button">View</a>
             <div class="pub-description">
               When a Retired Letter Carrier Dies...<br /> Step-by-step Instructions for Survivors to Ensure the Continuation of Benefits
             </div>
 
-
           </li>
+
           <li>
             <div class="pub-image">
-              <a href={pdf4} target="blank">
-                <Image fluid={data.pubActiveDies.childImageSharp.fluid} />
+              <a href="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/when-an-active-letter-carrier-dies.pdf" target="blank">
+                <img src="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/when-an-active-letter-carrier-dies.png" />
               </a>
             </div>
-            <a href={pdf4} target="blank" class="waves-effect waves-light btn-small pub-button">View</a>
-            <a href={pdf4} class="waves-effect waves-light btn-small pub-button" download>Download</a>
+            <a href="<?php echo get_site_url() ?>/wp-content/uploads/2021/07/when-an-active-letter-carrier-dies.pdf" target="blank" class="waves-effect waves-light btn-small pub-button">View</a>
             <div class="pub-description">
               When an Active Letter Dies...<br /> Step-by-step Instructions for Survivors to Ensure the Payment of Benefits
               </div>
@@ -91,93 +85,6 @@
 </div>
 
 
-
-
-
-
-<div class="container archive-spacer">
-    <?php
-        $ourCurrentPage = get_query_var('paged');
-        $post_args=array(
-            'post_type' => 'any', 
-            'post_status' => 'publish', 
-            'category_name' => 'cca',
-            'posts_per_page'           => 8,
-            'orderby'                  => 'date',
-            'order'                    => 'DESC',
-            'paged' => $ourCurrentPage
-        );
-    
-        $post_my_query = new WP_Query($post_args);
-
-        if( $post_my_query->have_posts() ) 
-        {
-            while ($post_my_query->have_posts()) : $post_my_query->the_post(); 
-
-            ?>
-
-            <div class="">
-            <div class="item">
-               
-                    <div class="row">
-                        <div class="item-news-contain"> 
-                        
-                        <div class="item-text-contain">
-                            <div>
-
-                                <div class="result-type">
-                                    <?php 
-                                        $post_type = get_post_type_object( get_post_type() ); 
-                                        echo $post_type->labels->singular_name.':';
-                                    ?>
-                                </div>
-
-                                <h2>
-                                    <a href="<?php the_permalink();?>">
-                                        <?php echo get_the_title( $post_my_query->ID );?>
-                                    </a>
-                                </h2>
-                               
-                                <p>
-                                    <?php echo get_the_excerpt( $post_my_query->ID ); ?>
-                                </p>
-                            </div> 
-                        </div>
-                            
-                        <div class="col l3 m12 s12 item-button-contain">
-                            <div class="details-btn">
-                                <a href="<?php the_permalink(); ?>">
-                                    <div class="waves-effect waves-light btn right">Read More</div>
-                                </a>
-                            </div>
-                        </div>
-                        </div>
-                            
-                    </div>
-                </div>
-              
-            </div>
-
-
-            <?php endwhile;  ?>
-            <div class="contain-pagination">
-                <?php echo paginate_links(array(
-                    'total' => $post_my_query->max_num_pages,
-                    // 'base'          => str_replace( 9999999999, '%#%', esc_url( get_pagenum_link( 9999999999 ) ) ),
-                    // 'format'        => '?paged=%#%',
-                    // 'current'       => max( 1, get_query_var( 'paged' ) ),
-                    // 'total'         => $wp_query->max_num_pages,
-                    'end_size'      => 0,
-                    'mid_size'      => 1,
-                    'prev_next'     => False
-                )); ?>
-            </div> 
-    <?php
-        }
-        wp_reset_query($post_my_query);
-    ?>
-
-</div>
 
 
 <?php 
