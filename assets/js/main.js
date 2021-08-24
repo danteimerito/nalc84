af5895b2b72e9b6f8fae420a33e15f1ed6d0ac30
danteimerito <danteimerito@gmail.com>
2021-08-23 23:46:21 -0400

    debugged nav indicators

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,17 +95,19 @@ for (var _i2 = 0, _arr = arr; _i2 < _arr.length; _i2++) {
   
 } 
 
-// nav indicators for custom post types
+// nav indicators for subsections and/or pages with pagination. 
 var referenceBtn = document.getElementById('trigger-1');
 var handbooksBtn = document.getElementById('handbooks-manuals');
 var documentsBtn = document.getElementById('documents');
 var minutesBtn = document.getElementById('meeting-minutes');
 var presidentsBtn = document.getElementById('presidents-report');
+var covid19Btn = document.getElementById('covid19');
 
 var handbooksBtnMobile = document.getElementById('handbooks-manuals-mobile');
 var documentsBtnMobile = document.getElementById('documents-mobile');
 var minutesBtnMobile = document.getElementById('minutes-mobile');
 var presidentsBtnMobile = document.getElementById('presidents-mobile');
+var covid19BtnMobile = document.getElementById('covid19-mobile');
 
 //handbooks&manuals
 if (url.includes('/handbooks_manuals/')) {
@@ -130,6 +132,12 @@ if (url.includes('/presidents-report/')) {
   local84Btn.classList.add('button-active');
   presidentsBtn.classList.add('button-active');
   presidentsBtnMobile.classList.add('current');
+}
+//covid19
+if (url.includes('/covid19/')) {
+  topicsBtn.classList.add('button-active');
+  covid19Btn.classList.add('button-active');
+  covid19BtnMobile.classList.add('current');
 }
 
 
