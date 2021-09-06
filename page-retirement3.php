1523cb4388986ae64b95209d56ec549ba6675ac6
danteimerito <danteimerito@gmail.com>
2021-09-05 22:33:51 -0400

    create dynamic content sections

--- page-retirement3.php
+++ page-retirement3.php
@@ -2,33 +2,17 @@
 <?php get_header(); ?>
 
 
-<?php
-        $ourCurrentPage = get_query_var('paged');
-        $post_args=array(
-            'post_type'                => array('retirement_content'),
-            'post_status'              => 'publish',
-            'posts_per_page'           => 1,
-            'orderby'                  => 'date',
-            'order'                    => 'DESC',
-            // 'paged'                    => $ourCurrentPage
-        );
-    
-        $post_my_query = new WP_Query($post_args);
-
-        if( $post_my_query->have_posts() ) 
-        {
-            while ($post_my_query->have_posts()) : $post_my_query->the_post(); 
-
-            ?>
-
-
 
 <div class="container nav-spacer">
       <div class="row">
         <h1>Retirement</h1>
 
         <div class="col l5 m11">
-            <?php the_field('page_left') ?>
+          <!-- <h3>The Office of Personnel Management</h3>
+          <p>The Office of Personnel Management (OPM) acts as the personnel office for retired letter carriers, and is the agency responsible for administering retirement, health and life insurance benefits. Before you call or write to OPM, be sure to have your Civil Service Annuity (CSA) number ready. OPM can be reached by retirees who have a CSA number by calling <a href="tel:888-767-6738">888-767-6738</a>  or <a href="tel:724-794-2005">724-794-2005</a>, 7:40 a.m.-5 p.m. Monday through Friday (Eastern time), or <a href="tel:202-606-0500">202-606-0500</a> in the Washington, DC, area. Mail your written inquiries to Office of Personnel Management, Retirement Operations Center, Boyers, PA 16017 (unless OPM has specified a different address for a specific action such as health benefits open season).</p> -->
+
+          
+
         </div>
 
         <div class="col l5 m11 offset-l1">
@@ -95,102 +79,6 @@
       </div>
     </div>
 </div>
-
-    <?php endwhile;  ?>
-             
-             <?php
-                 }
-                 wp_reset_query($post_my_query);
-             ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<footer class="page-footer">
-    <div class="container">
-        <div class="row">
-
-            <div class="col l4 m6 s12 footer-section mailing-address">
-                <div class="footer-icon-headers">
-                    <img class="icon-envelope" alt="envelope icon" src="<?php echo get_site_url(); ?>/wp-content/themes/nalc84/assets/images/icon-envelope.svg"/>
-                    <h4 class="white-text">Mailing Address</h4>
-                </div>
-                <p class="grey-text text-lighten-4 mailing-address">
-                    <?php the_field('mailing_address') ?>
-                </p>
-            </div>
-
-            <div class="col l4 m6 s12 footer-section phone-numbers">
-                <div class="footer-icon-headers">
-                    <img class="icon-phone" alt="phone icon" src="<?php echo get_site_url(); ?>/wp-content/themes/nalc84/assets/images/icon-phone.svg"/>
-                    <h4 class="white-text">Phone Numbers</h4>
-                </div>
-                <?php the_field('phone_numbers') ?>
-            </div>
-
-            <div class="col l3 m4 s12 footer-section office-hours">
-                <div class="footer-icon-headers">
-                    <img class="icon-clock" alt="clock icon" src="<?php echo get_site_url(); ?>/wp-content/themes/nalc84/assets/images/icon-clock.svg"/>
-                    <h4 class="white-text">Office Hours</h4>
-                </div>
-                <?php the_field('office_hours') ?>
-            </div>
-
-        </div>
-
-        <div class="row">
-            <a href="<?php echo get_site_url() ?>/privacy-policy">Privacy Policy</a>
-        </div>
-      
-    </div>
-    
-    <div class="footer-copyright">
-
-   
-        <div class="container footer-sub">
-            
-            <div class="nalc-copyright">Copyright © 2020 - <?php echo date("Y"); ?> NALC Branch 84</div>
-            <a href="<?php echo get_site_url() ?>/">
-                <img id="logo-sticky" src="<?php echo get_template_directory_uri() ?>/assets/images/nalc-logo-badge-type-2.svg" alt="NALC Logo" />
-            </a>
-        </div>
-    </div>
-</footer>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
